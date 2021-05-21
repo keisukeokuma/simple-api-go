004b0469450312dd1c9b9ebf36e8043246b37856
Keisuke Okuma <k_okuma@cross-c.co.jp>
2021-05-21 12:04:42 +0900

    レビュー内容の修正

--- cmd/main.go
+++ cmd/main.go
@@ -12,24 +12,25 @@ func main() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 
 	// /getにアクセスした場合、handleParamsを実行する
-	myRouter.HandleFunc("/get", handleParams)
-	myRouter.HandleFunc("/err", handleErr)
+	myRouter.HandleFunc("/get", echoParamHandler)
+	myRouter.HandleFunc("/err", echoBadRequestHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
 
 }
 
 // http.ResponseWriterは出力先、クエリを受け取る
-func handleParams(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "query：%s\n", r.URL.RawQuery)
+func echoParamHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.RawQuery
+	fmt.Fprintf(w, "query：%s\n", query)
 }
 
-func handleErr(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.RawQuery
-	if url == "" {
-		fmt.Fprintf(w, "%v\n", http.StatusBadRequest)
+func echoBadRequestHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.RawQuery
+	if query == "" {
+		http.Error(w, "Bad request", http.StatusBadRequest)
 	} else {
-		fmt.Fprintf(w, "query：%s\n", r.URL.RawQuery)
+		fmt.Fprintf(w, "query：%s\n", query)
 	}
 
 }
