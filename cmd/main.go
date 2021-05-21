2cb8c3dd748b0a163685ed7e6a2bd8ac3ab9f873
Keisuke Okuma <k_okuma@cross-c.co.jp>
2021-05-21 12:20:41 +0900

    レビュー内容の修正

--- cmd/main.go
+++ cmd/main.go
@@ -12,13 +12,13 @@ func main() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 
 	// /getにアクセスした場合、handleParamsを実行する
-	myRouter.HandleFunc("/get", handleParams)
+	myRouter.HandleFunc("/get", echoParamHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
 
 }
 
 // http.ResponseWriterは出力先、クエリを受け取る
-func handleParams(w http.ResponseWriter, r *http.Request) {
+func echoParamHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "query：%s\n", r.URL.RawQuery)
 }
