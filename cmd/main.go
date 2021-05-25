bcc55d756953dedaf79659ef4fce08b5dbd3dcdc
Keisuke Okuma <k_okuma@cross-c.co.jp>
2021-05-25 11:28:18 +0900

    query値の検査の実装

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
+	"gopkg.in/go-playground/validator.v9"
 )
+
+type checkId struct {
+	ID int `validate:"min=1,max=99"`
+}
 
 func main() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 
 	myRouter.HandleFunc("/get", echoParamHandler)
 	myRouter.HandleFunc("/err", echoBadRequestHandler)
+	myRouter.HandleFunc("/url_check", echoCheckIdHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
 
@@ -29,5 +36,21 @@ func echoBadRequestHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Bad request", http.StatusBadRequest)
 	} else {
 		fmt.Fprintf(w, "query：%s\n", query)
+	}
+}
+
+func echoCheckIdHandler(w http.ResponseWriter, r *http.Request) {
+	validate := validator.New()
+
+	id := r.URL.Query().Get("ID")
+	idInt, _ := strconv.Atoi(id)
+	query := checkId{
+		ID: idInt,
+	}
+
+	if err := validate.Struct(query); err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+	} else {
+		fmt.Fprintf(w, "ID：%v\n", idInt)
 	}
 }
